6a5009f20684f7294b7e05b1b45ec5e13d2270b4
mohammadreza 2104 <mohammadrezawolf2104@gmail.com>
2022-11-18 16:35:06 +0330

    create weather-app and finish the functionality and fetching api.

---
+++ src/api.js
@@ -0,0 +1,14 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '408862dd81msh13c775e9e2a7a42p1acaedjsne86c7e0989f9',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "bd84539977fd8dc3e7b9014cf3d96e31";
+
